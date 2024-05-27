ef9c14e9154155d3503a7a09a3a8a951e54a02a2
gian <giancluciano@gmail.com>
2024-05-27 15:59:51 -0300

    nn scratch

--- neural_network_from_scratch/models.py
+++ neural_network_from_scratch/models.py
@@ -12,27 +12,30 @@ class NeuralNetworkBase:
 
     def forward_relu(self, layer, input):
         # Rectifier activation
-        return maximum(0, dot(input, layer[0]) + layer[1])
+        return maximum(0, self.forward(layer, input))
 
     def forward_softmax(self, layer, input:List[List[int]]):
         # Softmax activation
-        output = dot(input, layer[0]) + layer[1]
+        # normalized distribution of probabilities
+        output = self.forward(layer, input)
         exp_output = exp(output - max(output, axis=1, keepdims=True))
         return  exp_output / sum(exp_output, axis=1, keepdims=True) #axis=1 if input is a batch
 
+    def get_random_layer(self, n_inputs, layer_neurons):
+        # create random weights and biases  
+        return [(0.1 * randn(n_inputs, layer_neurons)), zeros((1, layer_neurons))]
+
     def shuffle_neurons(self, layer):
+        # update weights in a random directions  
         layer[0] += 0.05 * randn(len(layer[0]), len(layer[0][0]))
         layer[1] += 0.05 * randn(1, len(layer[0][0]))
     
-    def get_random_layer(self, n_inputs, layer_neurons):
-            return [(0.1 * randn(n_inputs, layer_neurons)), zeros((1, layer_neurons))]
 
 class NeuralNetwork(NeuralNetworkBase):
 
     def __init__(self, layers_shape) -> None:
         self.layers_shape = layers_shape
         self.layers = []
-        self.best_weights_n_biases = []
     
     def predict(self, inputs):
         output = inputs
@@ -40,27 +43,28 @@ class NeuralNetwork(NeuralNetworkBase):
             output = self.forward_softmax(layer, output)
         return argmax(output, axis=1)
 
-    def train(self, inputs, targets, interations=10000):
-        output = inputs
+    def softmax_train(self, inputs, targets, interations=10000):
         n_inputs = len(inputs[0])
         for layer_neurons in self.layers_shape:
             self.layers.append(self.get_random_layer(n_inputs, layer_neurons))
-            output = self.forward_softmax(self.layers[-1], output)
             n_inputs = layer_neurons
         
-        loss = mean(-log(clip(output[range(len(output)), targets], 1e-7, 1-1e-7)))
-        
+        loss = 99
+        best_output = None
         for _ in range(interations):
             output = inputs
             current_layers = deepcopy(self.layers)
             n_inputs = len(inputs[0])
             for layer in current_layers:
-                self.small_shuffle_neurons(layer)
+                self.shuffle_neurons(layer)
                 output = self.forward_softmax(layer, output)
                 n_inputs = layer_neurons
 
             current_loss = mean(-log(clip(output[range(len(output)), targets], 1e-7, 1-1e-7)))
             if current_loss < loss:
-                print(f"new best loss {loss}")
+                
                 loss = current_loss
                 self.layers = deepcopy(current_layers)
+                best_output = deepcopy(output)
+        print(f"best loss {loss}") # Categorical cross-entropy
+        print(f"best accuracy {mean(argmax(best_output, axis=1) == targets)}")
--- neural_network_from_scratch/oop_nn.py
+++ neural_network_from_scratch/oop_nn.py
@@ -2,16 +2,30 @@
 # using OOO
 from numpy.random import seed
 from nnfs.datasets import vertical_data
+from matplotlib import pyplot
+import cProfile
 seed(0)
 
 from models import NeuralNetwork
 
-train_data = vertical_data(1000, 2)
+BATCH_LEN = 1000
+INTERATIONS = 10000
+N_CLASSES = 2
+
+train_data, y = vertical_data(BATCH_LEN, N_CLASSES)
+
+#pyplot.axis('equal')
+#pyplot.scatter(train_data[:,0], train_data[:,1], c=y, cmap='brg')
+#pyplot.show()
 test_data = vertical_data(10, 2)
 
 
-nn = NeuralNetwork(layers_shape = (2,64,64,2))
-nn.train(train_data[0], train_data[1])
-print(nn.predict(test_data[0]))
-print(test_data[1])
-#nn.calculate_loss()
+with cProfile.Profile() as pr:
+    nn = NeuralNetwork(layers_shape = (2,2,2, N_CLASSES)) # layers shape is a tuple of int (number of neurons for each layer)
+    nn.softmax_train(train_data, y, INTERATIONS)
+
+    import pstats
+    print(f"{pstats.Stats(pr).total_tt:.4} seconds, {pstats.Stats(pr).total_calls} function calls")
+    # best loss 0.11168550211891784
+    # best accuracy 0.955
+    # 6.963818391000001 seconds, 2412600 function calls
